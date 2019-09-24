e5047be82e41f556e066056fc96a9c0e6bef6817
James Barnard <james.barnard@reasondigital.com>
2019-09-24 11:50:14 +0100

    Init dist for pages deploy

---
+++ dist/precache-manifest.a07105071ee2621e1fc3b7886e035754.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "3303c71a3310db31f63d",
+    "url": "/js/templates.52ce5692.js"
+  },
+  {
+    "revision": "8bd709f31ff3ba0a67da",
+    "url": "/js/chunk-vendors.c29d5e95.js"
+  },
+  {
+    "revision": "3e56a1dff3bd96a97023",
+    "url": "/js/chunk-adea75fa.76a4aa94.js"
+  },
+  {
+    "revision": "e3bfb823b083755734cd",
+    "url": "/js/app.311292e2.js"
+  },
+  {
+    "revision": "6225f41f9be3216b11a38a1722958198",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3303c71a3310db31f63d",
+    "url": "/css/templates.78db2f14.css"
+  },
+  {
+    "revision": "e3bfb823b083755734cd",
+    "url": "/css/app.3980361d.css"
+  }
+];
